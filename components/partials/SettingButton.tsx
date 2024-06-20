17151bb3e1a6bbcb019a40e7d21bb163af1a1a05
Mohammad_shahzaib07 <shahzaib@quolam.com>
2024-06-20 06:08:39 +0530

    rendered all the options

--- components/partials/SettingButton.tsx
+++ components/partials/SettingButton.tsx
@@ -1,44 +1,50 @@
 import { fontHauora } from "@/constant/constant";
 import { Div, Icon, Text } from "react-native-magnus";
+import { iconFontFamilyType } from "react-native-magnus/lib/typescript/src/ui/icon/icon.type";
 
-function SettingButton() {
+type PropsType = {
+  iconName: string;
+  iconFamily: iconFontFamilyType;
+  title: string;
+  iconFontSize?: number;
+};
+function SettingButton(props: PropsType) {
+  const { iconName, iconFamily, title, iconFontSize = 32 } = props;
   return (
-    <Div mb={24}>
-      <Div
-        w="full"
-        h={64}
-        flexDir="row"
-        justifyContent="space-between"
-        alignItems="center"
-        borderBottomWidth={1}
-        borderBottomColor="#E0E0E0"
-      >
-        <Div flexDir="row">
-          <Icon
-            fontSize={42}
-            name="user"
-            fontFamily="EvilIcons"
-            color="#222222"
-            mr={12}
-          />
-          <Text
-            color="#222222"
-            fontWeight="400"
-            fontSize={18}
-            fontFamily={fontHauora}
-            lineHeight={24}
-          >
-            Personal Info
-          </Text>
-        </Div>
-
+    <Div
+      w="full"
+      h={64}
+      flexDir="row"
+      justifyContent="space-between"
+      alignItems="center"
+      borderBottomWidth={1}
+      borderBottomColor="#E0E0E0"
+    >
+      <Div flexDir="row">
         <Icon
-          fontSize={32}
-          name="chevron-right"
-          fontFamily="EvilIcons"
-          color="#898989"
+          fontSize={iconFontSize}
+          name={iconName}
+          fontFamily={iconFamily}
+          color="#222222"
+          mr={12}
         />
+        <Text
+          color="#222222"
+          fontWeight="400"
+          fontSize={18}
+          fontFamily={fontHauora}
+          lineHeight={24}
+        >
+          {title}
+        </Text>
       </Div>
+
+      <Icon
+        fontSize={32}
+        name="chevron-right"
+        fontFamily="EvilIcons"
+        color="#898989"
+      />
     </Div>
   );
 }
--- screens/settings/SettingsMainScreen.tsx
+++ screens/settings/SettingsMainScreen.tsx
@@ -1,8 +1,84 @@
 import { StyleSheet } from "react-native";
 import React from "react";
-import { Button, Div, Icon, Text } from "react-native-magnus";
+import { Div, Text } from "react-native-magnus";
 import { fontHauora } from "@/constant/constant";
 import SettingButton from "@/components/partials/SettingButton";
+
+const options = [
+  {
+    id: 1,
+    groupName: "General",
+    options: [
+      {
+        id: 1,
+        title: "Personal Info",
+        iconName: "user",
+        iconFamily: "EvilIcons",
+        iconFontSize: 38,
+      },
+      {
+        id: 2,
+        title: "Address",
+        iconName: "map-outline",
+        iconFamily: "Ionicons",
+      },
+      {
+        id: 3,
+        title: "Pets Profile",
+        iconName: "paw-outline",
+        iconFamily: "Ionicons",
+      },
+      {
+        id: 4,
+        title: "Push Notification",
+        iconName: "notifications-outline",
+        iconFamily: "Ionicons",
+      },
+    ],
+  },
+  {
+    id: 2,
+    groupName: "Order & Cases",
+    options: [
+      {
+        id: 1,
+        title: "Medicine Order",
+        iconName: "",
+        iconFamily: "",
+      },
+      {
+        id: 2,
+        title: "Cases",
+        iconName: "",
+        iconFamily: "",
+      },
+    ],
+  },
+  {
+    id: 3,
+    groupName: "Billing and Security",
+    options: [
+      {
+        id: 1,
+        title: "Payment",
+        iconName: "",
+        iconFamily: "",
+      },
+      {
+        id: 2,
+        title: "Change Password",
+        iconName: "",
+        iconFamily: "",
+      },
+      {
+        id: 3,
+        title: "Legal",
+        iconName: "",
+        iconFamily: "",
+      },
+    ],
+  },
+];
 
 const SettingsMainScreen = () => {
   return (
@@ -16,43 +92,30 @@ const SettingsMainScreen = () => {
       >
         Settings
       </Text>
-
-      <Text
-        fontWeight="400"
-        fontSize={18}
-        fontFamily={fontHauora}
-        lineHeight={24}
-        mb={6}
-      >
-        General
-      </Text>
-
-      <SettingButton />
-
-      <Text
-        fontWeight="400"
-        fontSize={18}
-        fontFamily={fontHauora}
-        lineHeight={24}
-        mb={8}
-      >
-        Order & Cases
-      </Text>
-      <Div mb={24}>
-        <Text>Options....</Text>
-      </Div>
-      <Text
-        fontWeight="400"
-        fontSize={18}
-        fontFamily={fontHauora}
-        lineHeight={24}
-        mb={8}
-      >
-        Billing and Security
-      </Text>
-      <Div mb={40}>
-        <Text>Options....</Text>
-      </Div>
+      {options.map((group) => (
+        <React.Fragment key={group.id}>
+          <Text
+            fontWeight="400"
+            fontSize={18}
+            fontFamily={fontHauora}
+            lineHeight={24}
+            mb={8}
+          >
+            {group.groupName}
+          </Text>
+          <Div mb={24}>
+            {group.options.map((option) => (
+              <SettingButton
+                key={option.id}
+                title={option.title}
+                iconName={option.iconName}
+                iconFamily={option.iconFamily}
+                iconFontSize={option?.iconFontSize}
+              />
+            ))}
+          </Div>
+        </React.Fragment>
+      ))}
 
       <Div mt="auto">
         <Text
