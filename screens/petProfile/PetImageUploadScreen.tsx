3abc2523b1baff5928632475b14b30ec5570eda1
Mohammad_shahzaib07 <shahzaib@quolam.com>
2024-06-25 23:17:37 +0530

    Upload three images screen completed

--- screens/petProfile/PetImageUploadScreen.tsx
+++ screens/petProfile/PetImageUploadScreen.tsx
@@ -8,14 +8,17 @@ const PetImageUploadScreen = () => {
       <Text fontSize={32} lineHeight={40} color="#222222" mb={16}>
         Upload three images
       </Text>
-      <Div mb={24}>
+      <Div mb={24} flexDir="row" style={{ gap: 12 }}>
         <ImageUpload isPrimary />
+        <ImageUpload />
+        <ImageUpload />
       </Div>
       <Text
         fontFamily={fontHauoraMedium}
         fontSize="md"
         lineHeight={20}
         color="#494949"
+        maxW={350}
       >
         Please make sure to upload three high-quality images in JPEG format so
         that the vet can clearly see your dog's profile.
