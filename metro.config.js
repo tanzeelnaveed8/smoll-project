eaf5c276bcc6ee22ab639c3aec9ee1924ff727af
farhanmian <farhanmian099@gmail.com>
2024-12-10 08:41:39 +0530

    fixing

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,24 @@
-const { getSentryExpoConfig } = require("@sentry/react-native/metro");
+// const { getSentryExpoConfig } = require("@sentry/react-native/metro");
 
-const config = getSentryExpoConfig(__dirname);
+// const config = getSentryExpoConfig(__dirname);
 
-module.exports = config;
+// module.exports = config;
+
+const { getDefaultConfig } = require("expo/metro-config");
+
+module.exports = (() => {
+  const config = getDefaultConfig(__dirname);
+
+  const { transformer, resolver } = config;
+
+  config.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve("react-native-svg-transformer"),
+  };
+  config.resolver = {
+    ...resolver,
+    sourceExts: [...resolver.sourceExts, "svg"],
+  };
+
+  return config;
+})();
