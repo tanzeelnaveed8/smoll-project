b072093ada9710f9470b095c78bdfca75dd8aee1
Mohib Arshi <mohibarshi834@gmail.com>
2025-02-18 08:41:54 +0530

    fix: android setup

--- android/app/src/main/java/com/seldom/small_friend/MainActivity.kt
+++ android/app/src/main/java/com/seldom/small_friend/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.anonymous.smoll
+package com.seldom.small_friend
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/seldom/small_friend/MainApplication.kt
+++ android/app/src/main/java/com/seldom/small_friend/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.anonymous.smoll
+package com.seldom.small_friend
 
 import android.app.Application
 import android.content.res.Configuration
